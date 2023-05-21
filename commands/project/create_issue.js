16055d6926b8f7ef04d183eb579e3ba0d8ccc436
Lucas dos Santos <santoslucasantos796@gmail.com>
2023-05-21 16:28:42 -0300

    #8 Adicionado suporte a projetos Github

--- commands/project/create_issue.js
+++ commands/project/create_issue.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
+const { Octokit } = require("@octokit/core");
 const JiraRegister = require("../../models/jiraRegister")
-const { ActionRowBuilder, AttachmentBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const GithubRegister = require("../../models/githubRegister")
 
 module.exports = {
     name: "createissue", // Coloque o nome do comando
@@ -8,34 +9,72 @@ module.exports = {
     type: Discord.ApplicationCommandType.ChatInput,
     options: [
         {
-            name: "dominio",
-            description: "Insira o dominio.",
-            type: Discord.ApplicationCommandOptionType.String,
-            required: true,
+            name: "jira",
+            description: "Cria uma issue em projeto Jira.",
+            type: Discord.ApplicationCommandOptionType.Subcommand,
+            options: [
+                {
+                    name: "dominio",
+                    description: "Insira o dominio.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: true,
+                },
+                {
+                    name: "projeto",
+                    description: "Insira o projeto.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: true,
+                },
+                {
+                    name: "titulo",
+                    description: "Insira o titulo.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: true,
+                },
+                {
+                    name: "tipo",
+                    description: "Insira o tipo de issue.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: true,
+                },
+                {
+                    name: "descrição",
+                    description: "Insira a descrição.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: false,
+                },
+            ],
         },
         {
-            name: "projeto",
-            description: "Insira o projeto.",
-            type: Discord.ApplicationCommandOptionType.String,
-            required: true,
-        },
-        {
-            name: "titulo",
-            description: "Insira o titulo.",
-            type: Discord.ApplicationCommandOptionType.String,
-            required: true,
-        },
-        {
-            name: "tipo",
-            description: "Insira o tipo de issue.",
-            type: Discord.ApplicationCommandOptionType.String,
-            required: true,
-        },
-        {
-            name: "descrição",
-            description: "Insira a descrição.",
-            type: Discord.ApplicationCommandOptionType.String,
-            required: false,
+            name: "github",
+            description: "Cria uma issue em projeto Github.",
+            type: Discord.ApplicationCommandOptionType.Subcommand,
+            options: [
+                {
+                    name: "dono",
+                    description: "Insira o dono do repositorio.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: true,
+                },
+                {
+                    name: "repositorio",
+                    description: "Insira o repositorio.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: true,
+                },
+                {
+                    name: "titulo",
+                    description: "Insira o titulo.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: true,
+                },
+                {
+                    name: "descrição",
+                    description: "Insira a descrição.",
+                    type: Discord.ApplicationCommandOptionType.String,
+                    required: false,
+                },
+            ],
         },
     ],
     run: async (client, interaction) => {
@@ -43,46 +82,80 @@ module.exports = {
         if (!interaction.member.permissions.has(Discord.PermissionFlagsBits.ManageGuild))
             return interaction.reply({ content: `Você não possui permissão para utilizar este comando.`, ephemeral: true })
         
-        const jira = await JiraRegister.findById(interaction.user.id)
+        interaction.reply("⏳ Criando sua issue...")
 
-        if(jira == null) return interaction.reply({ content: "Você não possui um token registrado, use o comando `/register` para isso.", ephemeral: true })
-        
-        const domain = interaction.options.getString("dominio")
-        const project = interaction.options.getString("projeto")
-        const title = interaction.options.getString("titulo")
-        let description = interaction.options.getString("descrição")
-        if(!description) description = null
-        const type = interaction.options.getString("tipo")
+        if (interaction.options.getSubcommand() === 'jira') {
 
-        const result = await fetch(`https://${domain}.atlassian.net/rest/api/2/issue`, {
-
-            method: "POST",
-
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Basic ${jira.token}`
-            },
-
-            // Enviar os dados no corpo da requisição
-            body: JSON.stringify({
-                "fields": {
-                    "project": {
-                        "key": project
-                    },
-                    "summary": title,
-                    "description": description,
-                    "issuetype": {
-                        "name": type
+            const jira = await JiraRegister.findById(interaction.user.id)
+    
+            if (jira == null) return interaction.editReply({ content: "Você não possui um token registrado, use o comando `/register` para isso.", ephemeral: true })
+            
+            const domain = interaction.options.getString("dominio")
+            const project = interaction.options.getString("projeto")
+            const title = interaction.options.getString("titulo")
+            let description = interaction.options.getString("descrição")
+            if (!description) description = null
+            const type = interaction.options.getString("tipo")
+    
+            const result = await fetch(`https://${domain}.atlassian.net/rest/api/2/issue`, {
+                
+                method: "POST",
+    
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Basic ${jira.token}`
+                },
+    
+                // Enviar os dados no corpo da requisição
+                body: JSON.stringify({
+                    "fields": {
+                        "project": {
+                            "key": project
+                        },
+                        "summary": title,
+                        "description": description,
+                        "issuetype": {
+                            "name": type
+                        }
                     }
-                }
-            }),
-        }).then((dados) => { 
-            console.log(dados)
-            interaction.reply("✅ Issue `" + title + "` criada com sucesso!")
-        }).catch((error) => {
-            console.log(error)
-            interaction.reply(`❎ | Algo deu errado.`)
-        });
+                }),
+            }).then((dados) => {
+                console.log(dados)
+                interaction.editReply("✅ Issue `" + title + "` criada com sucesso!")
+            }).catch((error) => {
+                console.log(error)
+                interaction.editReply(`❎ | Algo deu errado.`)
+            });
+        } else if (interaction.options.getSubcommand() === 'github') { 
 
+            const github = await GithubRegister.findById(interaction.user.id)
+    
+            if (github == null) return interaction.editReply({ content: "Você não possui um token registrado, use o comando `/register` para isso.", ephemeral: true })
+
+            const owner = interaction.options.getString("dono")
+            const repo = interaction.options.getString("repositorio")
+            const title = interaction.options.getString("titulo")
+            let description = interaction.options.getString("descrição")
+            if (!description) description = null
+    
+            const octokit = new Octokit({
+                auth: github.token,
+              })
+
+            const result = await octokit.request(`POST /repos/${owner}/${repo}/issues`, {
+                owner: owner,
+                repo: repo,
+                title: title,
+                body: description,
+                headers: {
+                  'X-GitHub-Api-Version': '2022-11-28'
+                }
+            }).then((dados) => {
+                interaction.editReply("✅ Issue `" + title + "` criada com sucesso!")
+            }).catch((error) => {
+                console.log(error)
+                interaction.editReply(`❎ | Algo deu errado.`)
+            });
+        }
     }
 }
